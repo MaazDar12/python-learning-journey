c5aa1b0dfe87ffaed4ef4ea5af67d29a3a43dbab
MaazDar12 <maaznaeemdar@gmail.com>
2025-09-27 15:15:25 +0500

    variables practice

---
+++ variables.py
@@ -0,0 +1,51 @@
+name="maaz"
+
+age=17
+height=5.5
+print("My name is ",name,", I am ",age,"years old and ",height,"meters tall")
+
+print("hello,world")
+
+# Assign 10 to x, 20 to y, and 30 to z in one line
+# Your code here:
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
+
+# Assign 100 to variables a, b, and c in one line
+# Your code here:
+a=b=c=100
+print(a)           
+print(b)
+print(c)
+print("maazkhan")
+#studentname,studentage,major= alice,20,computerscience
+#print(studentname,studentage,major)
+
+maaz=500
+gm=500
+print(maaz+gm)
+print(maaz%gm)
+print(maaz*gm)
+print(maaz==gm)
+print(maaz!=gm)
+print(maaz>=gm)
+
+#how to find the datatype of the variable through type
+print(type(gm))
+print(type(float("inf")))
+print(type(2.8))
+
+
+name="maaz"
+caste="dar"
+lastname="khan"
+print(name+lastname+caste)
+
+
+
+
+
+
+print("kalsoom")
